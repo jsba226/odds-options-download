457549a019252a64db014f492813f49e7d2ad9a9
mdenninghoff <mdenninghoff@users.noreply.github.com>
2015-04-03 18:40:37 -0400

    Update downloadSurfaceData.php
    Shows results of a historical volatility query.

--- downloadSurfaceData.php
+++ downloadSurfaceData.php
@@ -29,6 +29,10 @@ const PREG_DATE_YYYYMMDD = '/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/';
 // Format dates from the database with this style.
 const SQL_DATE_FORMAT = '%Y-%m-%d';
 
+// Volatility types, used in html form and parsing input.
+const VOLTYPE_HIST = 0;    // historical volatility.
+const VOLTYPE_IMPL = 1;    // implied volatility.
+
 // Start the session to setup cookies.
 session_start();
 
@@ -42,40 +46,26 @@ $errors = array();
 // Error Checking and Parsing _GET requests.
 
 // Extract dataType from _GET, or default to hvol.
-$dataType = isset($_GET['dataType']) && $_GET['dataType'] == 'ivol' ? 'ivol' : 'hvol';
+$dataType = isset($_GET['dataType']) && $_GET['dataType'] == VOLTYPE_IMPL ? VOLTYPE_IMPL : VOLTYPE_HIST;
 
-//// Extract termRadio from _GET or default to 1.
-//// This is the Short Term value.
-//$termRadio = 1;
-//if( isset($_GET['termRadio']))
-//{
-//    // Ensure that the values are integers between 1 and 6.
-//    if( (int)$_GET['termRadio'] >= 1 && (int)$_GET['termRadio'] <= 6)
-//    {
-//        $termRadio = (int)$_GET['termRadio'];
-//    }
-//}
-//// done getting short term.
-//
-//// Long Term, default to 2.
-//$termRadioLong = 2;
-//if( isset($_GET['termRadioL']))
-//{
-//    // Ensure that the values are integers between 2 and 6.
-//    if( (int)$_GET['termRadioL'] >= 2 && (int)$_GET['termRadioL'] <= 6)
-//    {
-//        $termRadioLong = (int)$_GET['termRadioL'];
-//    }
-//}
-//// done getting long term.
 
 // Get the volatility type.
-$volType = 14; // default to 2 week historical volatility.
+// @TODO: look into PHP's new filtering functions.
+$volType = array();
 if( isset($_GET['volType']))
 {
-    if( (int)$_GET['volType'] > 13 && (int)$_GET['volType'] <= 26 )
+    foreach($_GET['volType'] as $val)
     {
-        $volType = (int)$_GET['volType'];
+        // Make sure the volType is either an adjusted historical volatility.
+        if( $dataType == VOLTYPE_HIST && (int)$val > 13 && (int)$val <= 26 )
+        {
+            $volType[] = (int)$val;
+        }
+        // Or the volType is Implied volatility from 1 to 6 months.
+        else if( $dataType == VOLTYPE_IMPL && (int)$val >= -6 && (int)$val < 0 )
+        {
+            $volType[] = (int)$val;
+        }
     }
 }
 // done getting the volatility type.
@@ -132,6 +122,9 @@ if( isset($_GET['ticker']))
 // done error Checking and Parsing _GET requests.
 ////////////////////////////////////////////////////////////////////////////////
 
+// Get the mapping of historical volatility types to names.
+$hvolmap = get_hvolmap();
+
 /*
  * Print Page Head.
  */
@@ -149,6 +142,7 @@ if( isset($_GET['ticker']))
        /*background: linear-gradient(#444, #222) repeat scroll 0 0 #222;*/
        background-color: #444;
        font-family: Helvetica,Arial,sans-serif;
+       margin: 10px 20px 100px;
    }
    ul#errors{
        color:red;
@@ -170,7 +164,52 @@ if( isset($_GET['ticker']))
        color: #eee; background-color: #222;
    }
    
+   #preview {font-family: courier new, courier,monospace;
+             font-size: 12pt;
+             border-spacing: 0px;
+             border-left: solid 1px #ddd;
+             border-bottom: solid 1px #ddd;
+             width: 50%; }
+   #preview td, #preview th { padding: 0px 5px; border-style: solid; border-width: 1px 1px 0px 0px; border-color: #ddd; }
+   #preview td.float { text-align: right;  }
+   
+
+   
+   fieldset { border-color: goldenrod;}
+   
+   
+   
   </style>
+  <script type="text/javascript">
+      function showHvol()
+      {
+          var hvolChk = document.getElementById('dataTypeHvolChk');
+          if( ! hvolChk ){ alert('hvolChk is null'); return -1; }
+          
+          var ivolChk = document.getElementById('dataTypeIvolChk');
+          if( ! ivolChk ){ alert('ivolChk is null'); return -1; }
+          
+          var hvolFS = document.getElementById('hvolFS');
+          if( ! hvolFS ){ alert('hvolFS is null'); return -1; }
+          
+          var ivolFS = document.getElementById('ivolFS');
+          if( ! ivolFS ){ alert('ivolFS is null'); return -1; }
+          
+          // hvol has been checked.
+          if( hvolChk.checked )
+          {
+              hvolFS.style.display = '';
+              ivolFS.style.display = 'none';
+          }
+          else
+          {
+              hvolFS.style.display = 'none';
+              
+              ivolFS.style.display = '';
+          }
+          
+      }
+  </script>
  </head>
  <body>
   <?php
@@ -190,78 +229,43 @@ if( isset($_GET['ticker']))
       
       <fieldset>
        <legend>Data Type</legend>
-      <label><input type="radio" name="dataType" value="hvol" <?php echo $dataType == 'hvol' ? 'checked="true"' : ''; ?>/>
+       <label><input type="radio" id="dataTypeHvolChk" name="dataType" <?php echo 'value="'.VOLTYPE_HIST.'"'. ($dataType == VOLTYPE_HIST ? ' checked="true"' : ''); ?> onchange="showHvol()" />
        Historical Volatility</label><br>
-      <label><input type="radio" name="dataType" value="ivol" <?php echo $dataType == 'ivol' ? 'checked="true"' : ''; ?> />
+      <label><input type="radio" id="dataTypeIvolChk" name="dataType" <?php echo 'value="'.VOLTYPE_IMPL.'"'.($dataType == VOLTYPE_IMPL ? ' checked="true"' : ''); ?>  onchange="showHvol()" />
       Implied Volatility</label>
       </fieldset>
         
-      <fieldset>
-       <legend>Volatility Type</legend>
+      <fieldset id="hvolFS" <?php if($dataType == VOLTYPE_IMPL){echo 'style="display:none"';} ?> >
+       <legend>Historical Volatility</legend>
       <?php
-      // Code adapted from chart.php lines 261-281.
-      // @TODO: make this a function.
-    $mysqlq="select volType, name, details from eqhvolmap where volType > '13'"; //Only adjusted volatilities
-    $mysql=mysql_query($mysqlq);
-
-    while($row=mysql_fetch_row($mysql)) {
-       $id=$row[0];
-       $d1=preg_split("/,/",$row[2]); // split description at the comma so we can remove Textbook Hist volatility,"
-       $name=$d1[1];
-       
-       // If volatility type is 1 month or 1 year.
-       if($id == 15 || $id == 22) {
-          $name=preg_replace("/m/", " month", $name);
-          $name=preg_replace("/w/", " week", $name); // this line isn't necessary. md.
-          $name=preg_replace("/y/", " year", $name);
-          $name=preg_replace("/d/", " days", $name);
-       } else {
-          $name=preg_replace("/m/", " months", $name);
-          $name=preg_replace("/w/", " weeks", $name);
-          $name=preg_replace("/y/", " years", $name);
-          $name=preg_replace("/d/", " days", $name);
-       }
-       //Trim out the (X days)
-       $name=preg_replace('/ \W.*\W/', "", $name);
-       
-       echo '<label><input type="radio" name="volType" value="'.$id.'"';
-       echo $id == $volType ? ' checked="checked"' : '';
-       echo ">$name</label><br/>\n";
+    
+    
+    // Print checkboxes for each historical volatility type.
+    foreach( $hvolmap as $id => $name )
+    {
+        echo '<label><input type="checkbox" name="volType[]" value="'.$id.'"';
+        echo in_array($id, $volType) ? ' checked="checked"' : '';
+        echo ">$name</label><br/>\n";
     }
-    // done fetching volatility types.
       ?>
        </fieldset>
       
-      <!--
-      <fieldset>
-       <legend>Short Term</legend>
-       <input type="radio" name="termRadio" value="1" <?php echo $termRadio == 1 ? 'checked="true"' : ''; ?>/>
-       1 Month<br>
-       <input type="radio" name="termRadio" value="2" <?php echo $termRadio == 2 ? 'checked="true"' : ''; ?>/>
-       2 Month<br>
-       <input type="radio" name="termRadio" value="3" <?php echo $termRadio == 3 ? 'checked="true"' : ''; ?>/>
-       3 Month<br>
-       <input type="radio" name="termRadio" value="4" <?php echo $termRadio == 4 ? 'checked="true"' : ''; ?>/>
-       4 Month<br>
-       <input type="radio" name="termRadio" value="5" <?php echo $termRadio == 5 ? 'checked="true"' : ''; ?>/>
-       5 Month<br>
-       <input type="radio" name="termRadio" value="6" <?php echo $termRadio == 6 ? 'checked="true"' : ''; ?>/>
-       6 Month<br>
+      <fieldset id="ivolFS" <?php if($dataType == VOLTYPE_HIST){echo 'style="display:none"';} ?> >
+       <legend>Implied Volatility</legend>
+       <?php
+       // Output the 1 month first, since its label isn't plural.
+        echo '<label><input type="checkbox" name="volType[]" value="'.$i.'"';
+        echo in_array($i, $volType) ? ' checked="checked"' : '';
+        echo '>1 month</label><br/>'."\n";
+        // Output 2-6 months, using plural form of "months".
+       for($i=-2; $i >= -6; $i--)
+       {
+           echo '<label><input type="checkbox" name="volType[]" value="'.$i.'"';
+           echo in_array($i, $volType) ? ' checked="checked"' : '';
+           echo '>'.($i*-1).' months</label><br/>'."\n";
+       }
+       ?>
       </fieldset>
-      
-      <fieldset>
-       <legend>Long Term</legend>
-       <input type="radio" name="termRadioL" value="2" <?php echo $termRadioLong == 2 ? 'checked="true"' : ''; ?>/>
-       2 Month<br>
-       <input type="radio" name="termRadioL" value="3" <?php echo $termRadioLong == 3 ? 'checked="true"' : ''; ?>/>
-       3 Month<br>
-       <input type="radio" name="termRadioL" value="4" <?php echo $termRadioLong == 4 ? 'checked="true"' : ''; ?>/>
-       4 Month<br>
-       <input type="radio" name="termRadioL" value="5" <?php echo $termRadioLong == 5 ? 'checked="true"' : ''; ?>/>
-       5 Month<br>
-       <input type="radio" name="termRadioL" value="6" <?php echo $termRadioLong == 6 ? 'checked="true"' : ''; ?>/>
-       6 Month<br>
-      </fieldset>-->
 
       <fieldset>
        <legend>Date Range</legend>
@@ -280,66 +284,200 @@ if( isset($_GET['ticker']))
  * done printing page head.
  */
   
-//
-// Show a preview of the data:
-// Code from chart.php lines 261-281.
-
-// Note: Does term really mean volatility type? No. His requirements specify
-//       that the user chooses "term." However, the query asks for a volatility
-//       type. Also, there is a volatility type on the chart menu that matches 
-//       what this query expects.
-$mysqlq_str="SELECT eqp.date_, eqp.vol \n"
-    . "FROM eqhvol AS eqp LEFT JOIN eqmaster AS eqm ON eqm.eqId=eqp.eqId AND eqm.startDate >= '$startDate' and eqm.endDate <= '$endDate' \n"
-    . "WHERE eqm.ticker like '$ticker' AND eqp.date_<= now() \n"
-    . "  AND eqp.volType=$volType\n";
-
-
-// Query is adapted from chart.php Line 375.
-// @TODO: use constant for date format.
-// @TODO: see if this data is correct. Need to ask about it, because 
-// a query will return results even if there is no hvol.vol data. (because of left join).
-// changing to normal join fixes that. data starts whenever hvol data starts.
-$mysqlq_str="SELECT eqm.ticker, eqp.close_, hvol.vol, date_format(eqp.date_, '".SQL_DATE_FORMAT."')\n"
-. "FROM eqprice AS eqp LEFT JOIN eqmaster AS eqm ON eqm.eqId=eqp.eqId AND eqp.date_ between eqm.startDate AND eqm.endDate\n"
-. "  JOIN eqhvol AS hvol ON hvol.eqId=eqp.eqId AND hvol.volType=".$volType." AND hvol.date_=eqp.date_\n"
-. "WHERE eqm.ticker like '$ticker' AND eqp.date_>='$startDate' AND eqp.date_<='$endDate'\n"
-. "ORDER BY eqp.date_ ASC LIMIT 10";
-
-echo '<pre>'.$mysqlq_str.'</pre>';
-
-//Now, run query to get data
-//$mysqlq="SELECT eqp.date_, eqp.vol FROM eqhvol AS eqp LEFT JOIN eqmaster AS eqm ON eqm.eqId=eqp.eqId AND '$currentDate' between eqm.startDate and eqm.endDate WHERE eqm.ticker like '$ticker' AND eqp.date_<='$currentDate' AND eqp.volType=$v";
-$mysqlq=mysql_query($mysqlq_str);
-if($mysqlq)
+/*
+ * Page Body.
+ */
+if( $dataType == VOLTYPE_HIST )
 {
-    echo '<pre>';
-    while($row=mysql_fetch_array($mysqlq))
-    {
-        printf("%s %s %s %s\n", $row[0], $row[1], $row[2], $row[3]);
-    }
-    echo '</pre>';
+    print_hvoldata($ticker, $startDate, $endDate, $volType, $hvolmap);
 }
 else
 {
-    echo '<p>'.mysql_error()."</p>\n";
+//    prin
 }
 
-//$mysqlq="select * from eqhvolmap where volType = $v";
-//$mysql=mysql_query($mysqlq);
-//$row=mysql_fetch_array($mysql);
-//$details=$row[2];
-//$d1=preg_split("/,/",$details); 
-//$name=$d1[1];
-//$name=preg_replace("/w/", "week", $name);
-//$name=preg_replace("/y/", "years", $name);
-//$name=preg_replace("/d/", "days", $name);
-//$volName=$name;
-
-
+/*
+ * done printing page Body.
+ */
   
 /*
  * Print Page bottom.
  */
+
+// Debugging output to verify correctness of volType array. MD.
+//echo '<pre>'. print_r($_GET,true) . print_r($volType,true) . '</pre>';
+    
+
+/**
+ * Read each Historical Volatility type from the database. Only read the
+ * adjusted volatilities (volType > 13).
+ * 
+ * If the query fails, then $errors gets a descriptive error.
+ * 
+ * Code adapted from chart.php lines 261-281, written by Chris Moore,
+ * Fishback Management and Research 12/20/12.
+ * 
+ * @global array $errors
+ * @return array
+ * Returns an associative array with volType for array keys, and a formatted 
+ * name as the array values.
+ * 
+ * If the database query fails, then an empty array is returned.
+ */
+
+function get_hvolmap()
+{
+    global $errors;
+    
+    $harray = array();
+    
+    $mysqlq="select volType, name, details from eqhvolmap where volType > '13'";
+    $mysql=mysql_query($mysqlq);
+    if( $mysql)
+    {
+    
+    // Read each volatility type and reformat the description.
+    while($row=mysql_fetch_row($mysql)) {
+       $id=$row[0];
+       // split description at the comma so we can remove Textbook Hist volatility,"
+       $d1=preg_split("/,/",$row[2]); 
+       $name=$d1[1];
+       
+       // If volatility type is 1 month or 1 year, use singular month/year.
+       if($id == 15 || $id == 22) {
+          $name=preg_replace("/m/", " month", $name);
+          $name=preg_replace("/y/", " year", $name);
+       } else {
+          $name=preg_replace("/m/", " months", $name);
+          $name=preg_replace("/w/", " weeks", $name);
+          $name=preg_replace("/y/", " years", $name);
+          $name=preg_replace("/d/", " days", $name);
+       }
+       //Trim out the (X days)
+       $name=preg_replace('/ \W.*\W/', "", $name);
+       $harray[$id]= trim($name);
+    }
+    // done reading each eqhvolmap row.
+    }
+    else
+    {
+        $errors[] = mysql_error();
+    }
+    return $harray;
+}
+// end get_hvolmap().
+
+/**
+ * Given some parameters, query the database for historical volatility data.
+ * Print html out to the page with a html table with the data.
+ * 
+ * @param string $ticker
+ * @param string $startDate
+ * @param string $endDate
+ * @param int $volType
+ * @param array $hvolmap
+ */
+function print_hvoldata($ticker, $startDate, $endDate, $volType, $hvolmap)
+{
+    //
+    // Show a preview of the data:
+    // Code from chart.php lines 261-281.
+
+    // Note: Does term really mean volatility type? No. His requirements specify
+    //       that the user chooses "term." However, the query asks for a volatility
+    //       type. Also, there is a volatility type on the chart menu that matches 
+    //       what this query expects.
+    //       
+    //       // This is the original query I thought was correct. MD.
+//    $mysqlq_str="SELECT eqp.date_, eqp.vol \n"
+//        . "FROM eqhvol AS eqp LEFT JOIN eqmaster AS eqm ON eqm.eqId=eqp.eqId AND eqm.startDate >= '$startDate' and eqm.endDate <= '$endDate' \n"
+//        . "WHERE eqm.ticker like '$ticker' AND eqp.date_<= now() \n"
+//        . "  AND eqp.volType=$volType\n";
+
+
+    // Query is adapted from chart.php Line 375.
+    // @TODO: see if this data is correct. Need to ask about it, because 
+    // a query will return results even if there is no hvol.vol data. (because of left join).
+    // changing to normal join fixes that. data starts whenever hvol data starts.
+    $mysqlq_str="SELECT eqm.ticker, hvol.volType, eqp.close_, hvol.vol, \n"
+    . "date_format(eqp.date_, '".SQL_DATE_FORMAT."') AS eqp_date\n"
+    . "FROM eqprice AS eqp LEFT JOIN eqmaster AS eqm ON eqm.eqId=eqp.eqId AND eqp.date_ between eqm.startDate AND eqm.endDate\n"
+    . "  JOIN eqhvol AS hvol ON hvol.eqId=eqp.eqId\n"
+    //. " AND hvol.volType=".$volType."\n"
+    . "   AND hvol.volType IN (".  implode(',', $volType).")\n"
+    . "AND hvol.date_=eqp.date_\n"
+    . "WHERE eqm.ticker like '$ticker' AND eqp.date_>='$startDate' AND eqp.date_<='$endDate'\n"
+    . "ORDER BY eqp.date_, hvol.volType LIMIT 10";
+
+    echo '<pre>'.$mysqlq_str.'</pre>';
+
+    //$col_hdr = array('Ticker', 'Vol. Type', 'Price', 'Volatility', 'Date');
+    //$hdr_formats = array('%-6s', '%9s',     '%-10s',    '%-17s',      '%-10s');
+    //$col_formats = array('%-6s', '%-9s',       '%0.6f', '%0.15f',     '%s');
+    //$format_str = '| ' . implode(' | ', $col_formats) . " |\n";
+    //$hformat_str = '| ' . implode(' | ', $hdr_formats) . " |\n";
+
+    // Now, run query to get data
+    $mysqlq=mysql_query($mysqlq_str);
+    if($mysqlq)
+    {
+        echo '<table id="preview"><thead>'."\n";
+        echo ' <tr><th>Ticker</th>'."\n"
+                . '<th width="20%">Volatility Calculation Method</th>'."\n"
+                . '<th>Closing Price</th>'."\n"
+                . '<th>Realized Volatility</th>'."\n"
+                . '<th>Pricing Date</th>'."\n"
+                . '</thead></tr><tbody>'."\n";
+
+        while($row=mysql_fetch_array($mysqlq))
+        {
+            // Try to use the Hvol descriptive name instead of the numeric ID.
+            $hvolname = $row[1];
+            if( isset($hvolmap[$row[1]]) ) $hvolname = $hvolmap[$row[1]];
+
+            echo '<tr><td class="str">'.$row[0]."</td>\n"
+                    .'<td class="str">'.$hvolname."</td>\n"
+                    .'<td class="float">'.sprintf('%0.6f',$row[2])."</td>\n"
+                    .'<td class="float">'.sprintf('%0.15f',$row[3])."</td>\n"
+                    .'<td class="str">'.$row[4]."</td>\n"
+                    ."</tr>\n";
+        }
+        echo '</tbody></table>';
+    }
+    else
+    {
+        echo '<p>'.mysql_error()."</p>\n";
+    }
+}
+// end print_hvoldata().
+
+// @TODO: figure out the correct query. This one does not have moneyness in it.
+function print_ivoldata($ticker, $startDate, $endDate, $volType, $moneyness, $hvolmap)
+{
+    // Code from chart.php lines 272-283.
+    
+    //Now, run query to get data
+$mysqlq="SELECT eqp.date_, eqp.ivMid\n"
+. "FROM ivcmpr AS eqp LEFT JOIN eqmaster AS eqm ON eqm.eqId=eqp.eqId AND '$currentDate' between eqm.startDate and eqm.endDate\n"
+. "WHERE eqm.ticker like '$ticker'\n"
+. "  AND eqp.date_<='$currentDate'\n"
+. "  AND eqp.expiry=".abs($v)."\n"
+. "  AND eqp.strike=100";
+    
+    if($mysql=mysql_query($mysqlq)) {
+        $row=mysql_fetch_array($mysql);
+        $volOutput="[ [".(strtotime($row['date_'])*1000).", ".number_format($row['ivMid']*100, 4)." ]";
+        while($row=mysql_fetch_array($mysql)) {
+          $volOutput .= ", [".(strtotime($row['date_'])*1000).", ".number_format($row['ivMid']*100, 4)." ]\n";
+        }
+        $volOutput .= " ]";
+        
+        array_push($volOutArray, $volOutput);
+    }
+    $volName="Implied Volatility ".abs($v)."m";
+}
+// end print_ivoldata().
+
 ?>
  </body>
 </html>
